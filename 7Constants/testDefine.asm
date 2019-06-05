f8a554894c7d13f5cb47318f7b90b87269e62fad
Nathaniel Fisher <nathaniel.fisher@mygeorgian.ca>
2019-06-05 10:05:47 -0700

    add notes for constants and arithmetic, add code for adding 2 single digit numbers

---
+++ 7Constants/testDefine.asm
@@ -0,0 +1,17 @@
+%define PTR [EBP+4]
+section .text
+	global _start
+
+_start:
+	
+	mov  edx, 10
+	mov  ecx, msg
+	mov  ebx, 1
+	mov  eax, 4
+	int 0x80
+
+	mov  eax, 1
+	int  0x80
+
+section .data
+msg db PTR, 0xA, 0xD
